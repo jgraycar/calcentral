d8dd99bd6138625eddd42727685a7e0386fc6f1b
Joel Graycar <jgraycar@berkeley.edu>
2014-09-26 12:49:34 -0700

    moved isLawStudent from /my/status to /my/badges

--- app/models/my_badges/merged.rb
+++ app/models/my_badges/merged.rb
@@ -11,12 +11,22 @@ module MyBadges
     def initialize(uid, options={})
       super(uid, options)
       @now_time = Time.zone.now
+
+      # set law_student to true if user's only college is School of Law
+      profile_feed = Bearfacts::Profile.new({:user_id => @uid}).get
+      doc = profile_feed[:xml_doc]
+      @law_student = false
+      if !(doc.blank? || doc.css("studentGeneralProfile").blank?)
+        general_profile = doc.css("studentGeneralProfile")
+        @law_student = general_profile.css("collegePrimary").text.strip.eql? "LAW"
+      end
     end
 
     def get_feed_internal
       feed = {
         badges: get_google_badges,
-        studentInfo: StudentInfo.new(@uid).get
+        studentInfo: StudentInfo.new(@uid).get,
+        isLawStudent: @law_student
       }
       feed[:alert] = EtsBlog::Alerts.new.get_latest if Settings.features.app_alerts
       logger.debug "#{self.class.name} get_feed is #{feed.inspect}"
